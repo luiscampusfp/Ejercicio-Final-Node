27f7ab8b0f39a111eb2de97bd9e9e82540dbc8e7
Luis Miguel <luismiguel.molina@campusfp.es>
2023-02-20 14:33:40 +0100

    final

--- src/controllers/web.controller.js
+++ src/controllers/web.controller.js
@@ -26,14 +26,14 @@ class Controlador {
                 if (err) {
                     res.status(500).json({ mensaje: err.message });
                 } else {
-                    res.send(result[0]);
+                    res.send(result[0].total+"");
                 }
             });
         }
     }
 
     listaPedidos(req, res) {
-        connection.query("SELECT amount, payment_date FROM payment WHERE customer_id = " + req.body.id +" ORDER BY payment_id DESC LIMIT 10", function (err, result) {
+        connection.query("SELECT amount, payment_date FROM payment WHERE customer_id = " + req.body.id + " ORDER BY payment_id DESC LIMIT 10", function (err, result) {
             if (err) {
                 res.status(500).json({ mensaje: err.message });
             } else {
